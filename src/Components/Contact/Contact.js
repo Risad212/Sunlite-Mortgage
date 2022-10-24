e8d9887402db36a3d1285fc07724f9a525319974
Risad212 <hafez.risad@gmail.com>
2022-10-24 09:45:49 +0600

    done

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Contact.css';
-
+import ContactForm from '../Form/ContactForm';
 
 const Contact = () => {
     return (
@@ -58,43 +58,7 @@ const Contact = () => {
                         </div>
                     </Col>
                     <Col lg="6">
-                        <div className="form-container">
-                            <Form>
-                                <Form.Group className="mb-1" controlId="formBasicFName">
-                                    <Form.Label>First Name<span className='required'>*</span></Form.Label>
-                                    <Form.Control type="text" placeholder="" />
-                                </Form.Group>
-
-                                <Form.Group className="mb-1" controlId="formBasicLName">
-                                    <Form.Label>Last Name<span className='required'>*</span></Form.Label>
-                                    <Form.Control type="text" placeholder="" />
-                                </Form.Group>
-
-                                <Form.Group className="mb-1" controlId="formBasicEmail">
-                                    <Form.Label>Email<span className='required'>*</span></Form.Label>
-                                    <Form.Control type="email" placeholder="" />
-                                </Form.Group>
-
-                                <Form.Group className="mb-1" controlId="formBasicNumber">
-                                    <Form.Label>Contact Number<span className='required'>*</span></Form.Label>
-                                    <Form.Control type="tel" placeholder="(506) 234-5678" />
-                                </Form.Group>
-
-                                <Form.Group className="mb-1" controlId="formBasicOrganizationName">
-                                    <Form.Label>Your Organization</Form.Label>
-                                    <Form.Control type="text" placeholder="" />
-                                </Form.Group>
-
-                                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                                    <Form.Label>Message</Form.Label>
-                                    <Form.Control as="textarea" rows={3} />
-                                </Form.Group>
-
-                                <div className="formBtn">
-                                   <button className='button'>Submit</button>
-                                </div>
-                            </Form>
-                        </div>
+                      <ContactForm />
                     </Col>
                 </Row>
             </Container>
---
+++ src/Components/Form/ContactForm.js
@@ -0,0 +1,64 @@
+import React, { useRef } from 'react';
+import { Form } from 'react-bootstrap';
+import './ContactForm.css';
+import emailjs from '@emailjs/browser';
+
+const ContactForm = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_7qya74t','template_vde93za', form.current, 'cRBwueB5Z253m4eiD')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  }
+
+
+    return (
+        <>
+            <div className="form-container">
+                <Form ref={form} onSubmit={sendEmail}>
+                    <Form.Group className="mb-1" controlId="formBasicFName">
+                        <Form.Label>First Name<span className='required'>*</span></Form.Label>
+                        <Form.Control type="text" placeholder="" name="user_Fname"/>
+                    </Form.Group>
+
+                    <Form.Group className="mb-1" controlId="formBasicLName">
+                        <Form.Label>Last Name<span className='required'>*</span></Form.Label>
+                        <Form.Control type="text" placeholder="" name="user_lname"/>
+                    </Form.Group>
+
+                    <Form.Group className="mb-1" controlId="formBasicEmail">
+                        <Form.Label>Email<span className='required'>*</span></Form.Label>
+                        <Form.Control type="email" placeholder="" name="user_email"/>
+                    </Form.Group>
+
+                    <Form.Group className="mb-1" controlId="formBasicNumber">
+                        <Form.Label>Contact Number<span className='required'>*</span></Form.Label>
+                        <Form.Control type="tel" placeholder="(506) 234-5678" name="user_number"/>
+                    </Form.Group>
+
+                    <Form.Group className="mb-1" controlId="formBasicOrganizationName">
+                        <Form.Label>Your Organization</Form.Label>
+                        <Form.Control type="text" placeholder="" name="user_companyName"/>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Message</Form.Label>
+                        <Form.Control as="textarea" rows={3} name="user_message"/>
+                    </Form.Group>
+
+                    <div className="formBtn">
+                        <input className='button' type="submit" value="Submit" />
+                    </div>
+                </Form>
+            </div>
+        </>
+    );
+};
+
+export default ContactForm;
