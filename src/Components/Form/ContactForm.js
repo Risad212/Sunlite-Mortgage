90124b31551b898945fc44a6ccce715984bf4a2e
Risad212 <hafez.risad@gmail.com>
2022-11-01 15:16:53 +0600

    done

--- src/Components/Form/ContactForm.js
+++ src/Components/Form/ContactForm.js
@@ -1,24 +1,43 @@
-import Form from 'react-bootstrap'
+import { useState } from 'react'
+import {Form} from 'react-bootstrap'
 import './ContactForm.css'
 
 
 const ContactForm = () => {
+  const [formInfo,setFormInfo] = useState({
+    fname: '',
+    lname: '',
+    email: '',
+    number: '',
+    companyName: '',
+    message: '',
+  })
+
+  const handleOnChange = (e) => {
+    const newFormInfo = {...formInfo}
+    newFormInfo[e.target.name] = e.target.value
+    setFormInfo(newFormInfo)
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    const formElements = e.target.elements
-    const data = {
-      fname: formElements.fname.value,
-      lname: formElements.lname.value,
-      email: formElements.email.value,
-      number: formElements.number.value,
-      companyName: formElements.companyName.value,
-      message: formElements.message.value,
-    }
+    // method number 2
+    //-----------------------------
+    // const formElements = e.target.elements
+    // const data = {
+    //   fname: formElements.fname.value,
+    //   lname: formElements.lname.value,
+    //   email: formElements.email.value,
+    //   number: formElements.number.value,
+    //   companyName: formElements.companyName.value,
+    //   message: formElements.message.value,
+    // }
 
+  
     fetch('http://localhost:5000/send', {
       method: "POST",
       headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(data),
+      body: JSON.stringify(formInfo),
     })
     .then(res => console.log(res.json()))
     .then(err => console.log(err))
@@ -35,42 +54,38 @@ const ContactForm = () => {
             <Form.Label>
               First Name<span className="required">*</span>
             </Form.Label>
-            <Form.Control type="text" name="fname" />
+            <Form.Control type="text" name="fname" onChange={handleOnChange}/>
           </Form.Group>
 
           <Form.Group className="mb-1" controlId="formBasicLName">
             <Form.Label>
               Last Name<span className="required">*</span>
             </Form.Label>
-            <Form.Control type="text" name="lname" />
+            <Form.Control type="text" name="lname" onChange={handleOnChange}/>
           </Form.Group>
 
           <Form.Group className="mb-1" controlId="formBasicEmail">
             <Form.Label>
               Email<span className="required">*</span>
             </Form.Label>
-            <Form.Control type="email" name="email" />
+            <Form.Control type="email" name="email" onChange={handleOnChange}/>
           </Form.Group>
 
           <Form.Group className="mb-1" controlId="formBasicNumber">
             <Form.Label>
               Contact Number<span className="required">*</span>
             </Form.Label>
-            <Form.Control
-              type="tel"
-              placeholder="(506) 234-5678"
-              name="number"
-            />
+            <Form.Control type="tel" placeholder="(506) 234-5678" name="number" onChange={handleOnChange}/>
           </Form.Group>
 
           <Form.Group className="mb-1" controlId="formBasicOrganizationName">
             <Form.Label>Your Organization</Form.Label>
-            <Form.Control type="text" name="companyName" />
+            <Form.Control type="text" name="companyName" onChange={handleOnChange}/>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>Message</Form.Label>
-            <Form.Control as="textarea" rows={3} name="message" />
+            <Form.Control as="textarea" rows={3} name="message" onChange={handleOnChange}/>
           </Form.Group>
 
           <div className="formBtn">
