4b8efdb281d7bb588dfa72d73305083785bcaf3d
Risad212 <hafez.risad@gmail.com>
2022-11-06 16:32:47 +0600

    done

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import HeadTag from '../Components/HeadTag/HeadTag';
 import About from '../Layouts/About/About';
 import Banner from '../Layouts/banner/Banner';
 import Blog from '../Layouts/Blog/Blog';
@@ -16,14 +15,8 @@ const HomePage = () => {
       page: 'home'
     }
 
-    const HeadTagInfo = {
-      title: 'Home',
-      metaDis: 'Sunlite Mortgage Agents are some of the best trained mortgage agents in Canada and operate by the highest standard of mortgage brokering. In every situation',
-      metaKey: 'Meta keyword for HomePage',
-    }
     return (
         <div>
-           <HeadTag tagInfo={HeadTagInfo}/>
            <Banner page="home"/>
            <About abInfo={aboutInfo}/>
            <Service />
